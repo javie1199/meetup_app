141e1df04ba20babcab40de1ade0450ff5631b1f
Jon Tran <trungt8998@gmail.com>
2021-10-29 12:04:26 -0500

    Feat: Adding routing and css module

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,14 +1,16 @@
 import {Link} from 'react-router-dom'
+import classes from './MainNavigation.module.css'
 
 function MainNavigation() {
   return (
-    <div>
+    <header className={classes.header}>
+      <h1 className={classes.logo}>React Meetups</h1>
       <ul>
         <li><Link to='/'>All Meet up</Link></li>
         <li><Link to='/new-meetup'>New Meetup</Link></li>
         <li><Link to='/favorites'>Favorites</Link></li>
       </ul>
-    </div>
+    </header>
   )
 }
 
