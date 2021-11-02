38edb4689011814bd737116fad58e754d33f3f84
Jon Tran <trungt8998@gmail.com>
2021-11-01 21:56:05 -0500

    Working with add favorites

---
+++ src/store/favorites-context.js
@@ -0,0 +1,44 @@
+import { createContext, useState } from "react";
+
+// create a context this is JS object
+// capital F as this contains react component
+const FavoritesContext = createContext({
+  favorites: [],
+  totalFavorites: 0,
+});
+
+// provide info for all component need that info
+function FavoritesContextProvider(props) {
+  const [userFavorites, setUserFavorites] = useState([]);
+
+  function addFavoriteHandler(favoriteMeetup) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.concat(favoriteMeetup);
+    });
+  }
+
+  //filter return a new array that pass the condition
+  function removeFavoriteHandler(favoriteMeetupId) {
+    setUserFavorites((prevUserFavorites) => {
+      return prevUserFavorites.filter(
+        (meetup) => meetup.id !== favoriteMeetupId
+      );
+    });
+  }
+
+  //some return true or fasle in case one of element in a array pass the condition
+  function itemIsFavoriteHandler(meetupId) {
+    return userFavorites.some((meetup) => meetup.id === meetupId);
+  }
+
+  const context = {
+    favorites: userFavorites,
+    totalFavorites: userFavorites.length,
+  };
+
+  return (
+    <FavoritesContext.Provider value={context}>
+      {props.children}
+    </FavoritesContext.Provider>
+  );
+}
